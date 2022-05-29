f844e9ae3bde37b1bdf4c7cfa02ba4b9140c6981
Long <sis20132401@gmail.com>
2022-05-29 16:48:33 +0700

    init

---
+++ src/main/java/com/longnx/demojenkins/MainController.java
@@ -0,0 +1,15 @@
+package com.longnx.demojenkins;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Hello World!");
+    }
+
+}
